b874b7958fa95af06b703ab1fa27efc123d2b0df
Tomas <Hi>
2019-05-21 18:35:56 +0300

    Popup with user's info updated

--- src/components/Menu_Management/Zmones_Tab/PeopleListComponents/Popup/UserPopup.jsx
+++ src/components/Menu_Management/Zmones_Tab/PeopleListComponents/Popup/UserPopup.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Table } from 'react-bootstrap';
+
+
 
 import PropTypes from 'prop-types';
 
@@ -7,24 +9,87 @@ const UserPopup = props => {
   const { onToggle, isOpen, userInfo } = props;
 
   return (
-    <Modal show={isOpen} onHide={onToggle} className="Popup">
+    <Modal show={isOpen} onHide={onToggle} className="Popup" size = "lg" centered>
       <Modal.Header closeButton>
-        <Modal.Title> <h4>Profile of {userInfo.name} {userInfo.surname} </h4></Modal.Title>
+        <Modal.Title> 
+          <h4>
+            Profile of <b><i>{userInfo.name} {userInfo.surname}</i></b> 
+            <img src={userInfo.pic} alt="Profile pic" height="42" width="42" style={{borderRadius: "50%", marginLeft: "24px"}}/> 
+          </h4>
+        </Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <h4>'You got urself looking into</h4>
-        <h3> Mr. {userInfo.name} {userInfo.surname} </h3>
-        <hr/>
-        <h4> My pic {userInfo.pic} </h4>
-        <h4> Topic {userInfo.topic} </h4>
-        <h4> date {userInfo.date} </h4>
-        <h4> Hours {userInfo.hours} </h4>
+        <h5>Personal details:</h5>
+        <Table responsive bordered>
+          <tbody>
+            <tr>
+              <td width="25%"> E-mail address </td>
+              <td width="75%"> random@mail.com </td>
+            </tr>
+            <tr>
+              <td width="25%"> Status </td>
+              <td width="75%"> {userInfo.activity} </td>
+            </tr>
+          </tbody>
+        </Table>
+
+        <h5>Upcoming travels:</h5>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Start time</th>
+              <th>End time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>CHI-3000</td>
+              <td>2020-10-12</td>
+              <td>2021-10-12</td>
+            </tr>
+          </tbody>
+        </Table>
+
+        <h5>Past travels:</h5>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Start time</th>
+              <th>End time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>CHI-998</td>
+              <td>2010-10-10</td>
+              <td>2010-10-11</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>VNO-08</td>
+              <td>2010-10-16</td>
+              <td>2010-10-17</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>VNO-12</td>
+              <td>2010-10-10</td>
+              <td>2010-11-11</td>
+            </tr>
+          </tbody>
+        </Table>
         
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="secondary" onClick={onToggle}>
+        <Button variant="primary" onClick={onToggle}>
           Close window
         </Button>
       </Modal.Footer>
